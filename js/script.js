8e1f498a7f384b62700cd617d3c301a9d5ac4157
Glitch (cool-sudsy-bongo) <none>
2021-03-09 01:56:06 +0000

    🌴🛫 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 let playlist=["Song1","Song2","Song3"]
-let songArtists=["Conan Gray","Clinton Kane","Zach Hood"]
+let songArtist=["Conan Gray","Clinton Kane","Zach Hood"]
 let songLength=["2:33","3:43","4:06"]
 let songImage=[]
 let songLink=[]
 
-// loop over each array
-// append it to the .display div
+
+  
+  for(let song of playlist) {
+    $(".display").append(song + " ") 
+  }
+  for(let artist of songArtist){
+    $(".display").append(artist + " ")
+  }
+  for(let length of songLength) {
+   $(".display").append(length + " ")
+ }
