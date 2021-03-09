cb528d42efdd22d7a4593ac2554c62c80a273901
Glitch (cool-sudsy-bongo) <none>
2021-03-09 01:20:24 +0000

    👍📼 Updated with Glitch

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let playlist=["Song1","Song2","Song3"]
+let songArtists=["Conan Gray","Clinton Kane","Zach Hood"]
+let songLength=["2:33","3:43","4:06"]
+let songImage=[]
+let songLink=[]
+
+// loop over each array
+// append it to the .display div
